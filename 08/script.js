d10f32bb65edf40e872fa15b43238b14769c143e
Vasil Vanchuk <v.vanchuk@tut.by>
2016-01-09 18:20:27 +0300

    Schedulers

--- 08/script.js
+++ 08/script.js
@@ -44,3 +44,64 @@ setTimeout(() => {
 // => Value published to observer #2: 3
 // => onCompleted
 // => onCompleted
+
+// http://xgrommx.github.io/rx-book/content/getting_started_with_rxjs/scheduling_and_concurrency.html
+// Using Schedulers
+/*
+ A scheduler controls when a subscription starts and when notifications are published.
+ - ImmediateScheduler will start the specified action immediately
+ - CurrentThreadScheduler will schedule actions to be performed on the thread that makes the original call
+ - DefaultScheduler will schedule actions to be performed on a asynchronous callback,
+ which is optimized for the particular runtime, such as setImmediate or process.nextTick on Node.js
+ or in the browser with a fallback to setTimeout
+ */
+Rx.Observable.generate(
+  0,
+  () => true,
+  x => x + 1,
+  x => x
+  )
+  .observeOn(Rx.Scheduler.default)
+  .subscribe(
+    console.log.bind(console)
+  );
+/*
+ Instead of using the observeOn operator to change the execution context on which the observable sequence produces messages,
+ we can create concurrency in the right place to begin with.
+ */
+Rx.Observable.generate(
+  0,
+  () => true,
+  x => x + 1,
+  x => x,
+  Rx.Scheduler.default)
+  .subscribe(
+    console.log.bind(console)
+  );
+/*
+ It is best to place observeOn later in the query. This is because a query will potentially filter out a lot of messages,
+ and placing the observeOn operator earlier in the query would do extra work on messages that would be filtered out anyway.
+ Calling the observeOn operator at the end of the query will create the least performance impact.
+ */
+/*
+//Another advantage of specifying a scheduler type explicitly is that you can introduce concurrency for performance purpose, as illustrated by the following code.
+ seq.groupBy(...)
+  .map(x => x.observeOn(Rx.Scheduler.default))
+  .map(x => expensive(x))  // perform operations that are expensive on resources
+*/
+
+/* CHEATSHEET : When to Use Which Scheduler
+Scenario	                  => Scheduler
+Constant Time Operations	  => Rx.Scheduler.immediate
+Tail Recursive Operations	  => Rx.Scheduler.immediate
+Iteration Operations	      => Rx.Scheduler.currentThread
+Time-based Operations	      => Rx.Scheduler.default
+Asynchronous Conversions	  => Rx.Scheduler.default
+Historical Data Operations  =>	Rx.HistoricalScheduler
+Unit Testing	              => Rx.TestScheduler
+*/
+
+
+
+
+
